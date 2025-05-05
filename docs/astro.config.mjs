5bacb270a213de3ff4a45b4fdad58818720c95cd
pelikhan <jhalleux@microsoft.com>
2025-05-05 21:44:07 +0000

    enable pagination?

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
     integrations: [
         starlight({
             title: "PromptPex",
+            pagination: true,
             plugins: [
                 starlightLlmsTxt({
                     pageSeparator: "\n\n=|=|=|=|=|=\n\n",
