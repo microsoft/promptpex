827f8986d9864245c334fad7f17006550521cba7
Ben Zorn <zorn@microsoft.com>
2025-05-19 23:48:06 +0000

    :sparkles: Refine chatbot eval, rules, and documentation
    Enhanced tests, rules, and docs for better interaction clarity.

--- samples/run-samples.mjs
+++ samples/run-samples.mjs
@@ -4,7 +4,7 @@ import { $ } from "zx";
 import path from "path";
 
 // List of 8 .prompty files to substitute for speech-tag.prompty
-const promptyFilesAll = [
+const promptyFilesAll_ = [
     "samples/speech-tag/speech-tag.prompty",
     "samples/text-to-p/text-to-p.prompty",
     "samples/openai-examples/elements.prompty",
@@ -15,7 +15,7 @@ const promptyFilesAll = [
     "samples/azure-ai-studio/shakespearean-writing-assistant.prompty",
 ];
 
-const promptyFilesAll_ = [
+const promptyFilesAll = [
 
     "samples/speech-tag/speech-tag.prompty",
 
@@ -29,7 +29,9 @@ const outDir = `evals/test-all-${dateStr}`;
 for (const prompty of promptyFilesAll) {
     const promptyFileBase = path.basename(prompty, path.extname(prompty));
 
-    await $`npm run promptpex ${prompty} -- --vars "splitRules=true" --vars "maxRulesPerTestGeneration=5" --vars "testGenerations=1" --vars "evals=true" --vars "testExpansions=0" --vars "compliance=true" --vars "baselineTests=false" --vars "modelsUnderTest=ollama:llama3.2:1b" --vars "out=${outDir}/${promptyFileBase}"`;
+    await $`npm run promptpex ${prompty} --  --vars \"effort=medium\" --vars \"evals=true\" --vars \"compliance=true\" --vars \"baselineTests=false\"  --vars \"modelsUnderTest=azure:gpt-4o-mini_2024-07-18;ollama:gemma2:9b;ollama:qwen2.5:3b;ollama:llama3.2:1b\" --vars "out=${outDir}/${promptyFileBase}"`;
 }
 
 //    await $`npm run promptpex ${prompty} -- --vars "splitRules=true" --vars "maxRulesPerTestGeneration=5" --vars "testGenerations=1" --vars "evals=true" --vars"testExpansions=0" --vars "compliance=true" --vars baselineTests=false --vars "modelsUnderTest=azure:gpt-4o-mini_2024-07-18;ollama:gemma2:9b;ollama:qwen2.5:3b;ollama:llama3.2:1b" --vars "out=${outDir}/${promptyFileBase}"`;
+
+//    await $`npm run promptpex ${prompty} -- --vars "splitRules=true" --vars "maxRulesPerTestGeneration=5" --vars "testGenerations=1" --vars "evals=true" --vars "testExpansions=0" --vars "compliance=true" --vars "baselineTests=false" --vars "modelsUnderTest=ollama:llama3.2:1b" --vars "out=${outDir}/${promptyFileBase}"`;
