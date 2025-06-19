fb74bcd00201f608b1e5195e2aa2d78ad6851ecc
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-06-19 14:38:35 +0000

    use .bin path

--- bin.mjs
+++ bin.mjs
@@ -3,19 +3,11 @@ import { dirname, join } from "path"
 import { fileURLToPath } from "url"
 import { spawnSync } from "child_process"
 
+const cmd = "node_modules/.bin/genaiscript"
 const args = process.argv.slice(2)
 const scriptDir = dirname(fileURLToPath(import.meta.url))
 if ("configure" === args[0]) {
-    const result = spawnSync("genaiscript", args, { stdio: "inherit" })
-    if (result.error || result.status !== 0) process.exit(1)
-} else if ("serve" === args[0]) {
-    const genaiArgs = [
-        "serve",
-        //join(scriptDir, "src", "genaisrc", "promptpex.genai.mts"),
-        ...args.slice(1)
-    ]
-    console.error(`genaiscript ${genaiArgs.join(" ")}`)
-    const result = spawnSync("genaiscript", genaiArgs, { stdio: "inherit" })
+    const result = spawnSync(cmd, args, { stdio: "inherit" })
     if (result.error || result.status !== 0) process.exit(1)
 } else {
     const genaiArgs = [
@@ -25,7 +17,7 @@ if ("configure" === args[0]) {
         "--no-run-trace",
     ]
     console.error(`genaiscript ${genaiArgs.join(" ")}`)
-    const result = spawnSync("genaiscript", genaiArgs, { stdio: "inherit" })
+    const result = spawnSync(cmd, genaiArgs, { stdio: "inherit" })
     if (result.error)
         console.error(result.error.message)
     if (result.error || result.status !== 0) process.exit(1)
